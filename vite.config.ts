e610ebdb021ca7e658d507be21d30ddab1c06c3c
CJ Conti <cjc2818cjc@gmail.com>
2026-09-07 19:52:58 -0400

    Updated vite and packages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/summer-2026-weather/', // match your repo name exactly
   plugins: [react()],
+  build: {
+    outDir: 'build' // Optional — only if you want `build` instead of `dist`
+  },
 })
